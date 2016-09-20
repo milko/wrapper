bd450de17556d2abc703c40bdc033d616f510551
Milko Škofič <skofic@gmail.com>
2016-09-20 18:33:11 +0200

    Updated Phan configuration.
    Added batch directory.

--- .phan/config.php
+++ .phan/config.php
@@ -52,6 +52,7 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
+		'batch',
 		'src',
 		'vendor/mongodb/mongodb/src',
 		'vendor/mongodb/mongodb/src/Exception',
